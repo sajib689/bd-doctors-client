0487c0e09a1468a848c0932a2e6f7b908f29a876
sajib689 <jibonraja11@gmail.com>
2023-09-11 11:08:51 +0600

    d

--- src/Layout/Home/Catgory/Category.jsx
+++ src/Layout/Home/Catgory/Category.jsx
@@ -1,8 +1,10 @@
+import { AuthContext } from "../../../Providers/AuthProviders";
 import CategoryCard from "../CategoryCard/CategoryCard";
-import {  useEffect, useState } from "react";
+import {  useContext, useEffect, useState } from "react";
 
 const Category = () => {
     const [categories, setCategories] = useState([])
+    const {loading} = useContext(AuthContext)
   useEffect(() => {
     fetch('https://bd-doctors-server.vercel.app/services')
     .then(res => res.json())
@@ -13,6 +15,9 @@ const Category = () => {
       <div className="text-center font-bold text-4xl">
         <h1>Get Appointment</h1>
       </div>
+      {
+        loading()
+      }
       <div className="mt-20 grid grid-cols-3 gap-4">
         
         {categories.map(category => 
